2087bed99a29ee38b478e6e0d7ac197b61d197fc
Julie Troncone <jntroncone@gmail.com>
2021-03-24 11:51:06 -0500

    cleaning up server side
    fixed error for removing connections, added display_open_connections()

--- Server.py
+++ Server.py
@@ -20,49 +20,59 @@ class Server:
         print('IP: ' + str(host))
         print('Port: ' + str(port))
 
-
         while True:
             c, addr = self.s.accept()
             c.send("NAME".encode("utf-8"))
-            username = c.recv(1024).decode("utf-8")
+            username = (str(c.recv(1024).decode("utf-8")) + " at " + str(addr[1]))
+            # uses port number as unique identification number to distinguish between users
 
-            print('New connection. Username: ' + str(username))
-            self.message_all(f"{username} has joined, welcome!")
-
+            # new connection notice to all
+            print('New connection. Username: ' + username)
             self.usernames.append(username)
-
             self.clients.append(c)
+            self.message_all(f"{username} has joined, welcome!".encode("utf-8"))
 
-            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
+            threading.Thread(target=self.handle_client, args=(c, addr)).start()
 
-            self.message_all(f"Currently {threading.activeCount()-1} active users:\n")
+            self.message_all(f"Currently {threading.activeCount()-1} active users:\n".encode("utf-8"))
+            self.display_open_connections()
 
-            for user in self.usernames:
-                self.message_all(f"{user} \n")
-
-    def message_all(self, message):
-        for connection in self.clients:
-            connection.send(message.encode("utf-8"))
+            for client in self.clients:
+                print(client)
 
     def handle_client(self, c, addr):
         while True:
             try:
                 message = c.recv(1024)
+
             except:
-                c.shutdown(socket.SHUT_RDWR)
+                # connection closed, remove from lists and provide updated list
+                c.close()
+                for client in self.clients:
+                    if c == client:
+                        i = self.clients.index(client)
 
-                print(str(self.usernames[c]) + ' left...')
-                self.message_all(str(self.usernames[c]) + ' left...')
+                        self.clients.pop(i)
+                        self.usernames.pop(i)
 
-                self.clients.remove(c)
-                self.usernames.remove(c)
+                self.message_all("Someone left, here are the remaining users:".encode("utf-8"))
+                self.display_open_connections()
                 break
+            # if message.decode("utf-8").startswith("@"):
 
             if message.decode("utf-8") != '':
                 print(str(message.decode("utf-8")))
-                for connection in self.clients:
-                    # if connection != c:
-                    connection.send(message)
+                self.message_all(message)
+
+    def message_all(self, message):
+        for connection in self.clients:
+            connection.send(message)
+
+    def display_open_connections(self):
+        # send current active user list to all users
+        for user in self.usernames:
+            self.message_all(f"{user} \n".encode("utf-8"))
+            print(user)
 
 
 server = Server()
